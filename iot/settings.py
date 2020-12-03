05d9bd0947fe07784aa3e38da52eb66f55df7376
Lucas Araujo <lucas.araujo@lais.huol.ufrn.br>
2020-12-02 21:42:37 -0300

    Change database configuration

--- iot/settings.py
+++ iot/settings.py
@@ -88,10 +88,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'iot',
-        'USER': 'postgres',
-        'PASSWORD': 'postgres',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        # 'USER': 'postgres',
+        # 'PASSWORD': 'postgres',
+        # 'HOST': 'localhost',
+        # 'PORT': '5432',
     }
 }
 
